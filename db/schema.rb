1515537cd4cfd3a0d7572e311a821a2b02afca68
LoboArkano <pipiolo_mananes@hotmail.com>
2021-04-13 17:07:40 -0500

    Create buyers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_202318) do
+ActiveRecord::Schema.define(version: 2021_04_13_220408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buyers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
+    t.decimal "wallet", precision: 5, scale: 2, default: "50.0"
+    t.index ["email"], name: "index_buyers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
+  end
 
   create_table "sellers", force: :cascade do |t|
     t.string "email", default: "", null: false
