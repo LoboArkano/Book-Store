452aff64d036318d6914dd59cc6d24224e5ef6fc
LoboArkano <pipiolo_mananes@hotmail.com>
2021-04-14 15:58:57 -0500

    Create Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_181428) do
+ActiveRecord::Schema.define(version: 2021_04_14_205212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,14 @@ ActiveRecord::Schema.define(version: 2021_04_14_181428) do
     t.index ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
   end
 
+  create_table "items", force: :cascade do |t|
+    t.bigint "buyer_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
+  end
+
   create_table "sellers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +89,5 @@ ActiveRecord::Schema.define(version: 2021_04_14_181428) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "books", "sellers"
+  add_foreign_key "items", "buyers"
 end
