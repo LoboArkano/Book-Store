adece2f729d2365f7b442809ddd49208e3ddfa06
LoboArkano <pipiolo_mananes@hotmail.com>
2021-04-14 11:49:43 -0500

    Add book routes - Update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   devise_for :buyers, controllers: {
     registrations: 'registrations'
   }
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
     registrations: 'registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'books#index'
 end
