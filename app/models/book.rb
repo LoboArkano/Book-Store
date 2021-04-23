2d313f31921b700384794c161db4e754061bd3cd
LoboArkano <pipiolo_mananes@hotmail.com>
2021-04-22 23:52:35 -0500

    Implement search bar

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,14 @@ class Book < ApplicationRecord
   validates :title, :description, :author, :price, :stock, presence: true
   validates :title, length: { maximum: 150 }
   validates :description, length: { maximum: 1000 }
+
+  def self.search(search)
+    if search
+      books = Book.all.select { |book| /#{search}/i.match(book.title) }
+      books ||= Book.all.order('created_at desc')
+      books
+    else
+      Book.all.order('created_at desc')
+    end
+  end
 end
